910d74a161c7c58f2d7d98258657fe818a5dd4af
borisngong <borisngong@gmail>
2023-11-30 18:21:12 -0800

    a program that imports the variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import hidden_4
-
+from hidden_4 import hidden 4
 if __name__ == "__main__":
     names = dir(hidden_4)
     sorted_names = sorted(name for name in names if not name.startswith("__"))
