ab98009960288e7cc130d050d1e6efb3a34a9bf4
borisngong <borisngong@gmail>
2023-12-04 14:00:58 -0800

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     temporal_list = my_list[:]
-    if 0 <= idx or idx < len(my_list):
+    if 0 <= idx < len(my_list):
         temporal_list[idx] = element
         return temporal_list
     return my_list
