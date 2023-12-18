e21821d77032c9cd4d76fa5a060c6a636c951ae0
borisngong <borisngong@gmail>
2023-12-18 15:55:51 -0800

    Python fuuntion that does exactly the same as the  Python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
+
+    for i in range(1, 4):
         try:
             if i > a:
-                raise Exception('Too far')
-            result += a ** b / i
+                raise ValueError("Too far")
+            result += (a ** b) / i
         except ValueError:
-            continue
-    result += a + b
+            result = b + a
+            break
+
     return result
