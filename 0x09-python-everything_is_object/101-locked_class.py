722aab80956f17a9e09d436a0ae41cc8d968b10d
borisngong <borisngong@gmail>
2024-01-05 16:01:54 -0800

    A class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,10 +3,7 @@
 
 
 class LockedClass:
-    allowed_attributes = ["first_name"]
-
     def __setattr__(self, name, value):
-        if name not in self.allowed_attributes:
-            error_message = "'LockedClass' object has no attribute '{}'"
-            raise AttributeError(error_message.format(name))
+        if name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
         super().__setattr__(name, value)
