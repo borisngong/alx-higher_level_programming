a3a1d941b7f08090fe222f75d43bcad575e850a3
borisngong <borisngong@gmail>
2024-01-16 09:05:00 -0800

    Class Rectangle and attributes validated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,9 +6,8 @@ from models.base import Base
 class Rectangle(Base):
     """Class Rectangle inheriting from Base"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0):
         """Class constructor: Initializes attributes width, height, x, y"""
-        super().__init__()
         self.width = width
         self.height = height
         self.x = x
