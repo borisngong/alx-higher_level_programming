fd40c151334cc6b46883e0b251cd1fb0846a8229
borisngong <borisngong@gmail>
2024-03-27 16:23:07 -0700

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument."
-  exit 1
-fi
-
-url="$1"
-
-# Send request and get the size of the response body
-
-body_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}')
-
-# Display the size
-echo "$size"
+#A Bash script that takes in a URL, sends a request to that URL, and displays response size
+curl -sI "$1" | grep -i "content-length" | awk '{print $2}'
