7e2582f3eeddcbeae3e8ddd20c9d8eb66c2af392
borisngong <borisngong@gmail>
2024-03-27 16:48:26 -0700

    Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request, and displays response body
-curl -sfL "$1" -X GET
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
