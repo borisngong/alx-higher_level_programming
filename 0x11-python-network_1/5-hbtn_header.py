2412bbc12109690df96c4f5f97670705c41402d8
borisngong <borisngong@gmail>
2024-03-28 18:49:09 -0700

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,14 @@
 """
 Responsible for displaying the X-Request-Id header of a request to a given URL
 """
-import sys, requests
+import sys
+import requests
 
 
 if __name__ == "__main__":
+
     url = sys.argv[1]
 
     response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    x_request_id = response.headers.get("X-Request-Id")
+    print(x_request_id)
