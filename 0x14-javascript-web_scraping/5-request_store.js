9e9efb6f5ed43defde04f4fc96f6b789d1d2b688
borisngong <borisngong@gmail>
2024-04-24 14:26:11 -0700

    A script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+// Import the native Node.js 'fs' module
+const fs = require('fs');
+
+// Import the 'request' module
+const request = require('request');
+
+// Utilize the 'request' module to execute an HTTP GET request to the URL
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
