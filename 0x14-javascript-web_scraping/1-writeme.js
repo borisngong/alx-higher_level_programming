91fb346da962ded5b4ccc3c351bfde52eb2b6851
borisngong <borisngong@gmail>
2024-04-23 17:14:24 -0700

    A script that display the status code of a GET request

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
+#!/usr/bin/node
+
+const fs = require("fs");
 // Import the built-in Node.js 'fs' module.
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
+fs.writeFile(process.argv[2], process.argv[3], "utf8", (error) => {
   // Write data to a file specified as the third command-line argument (process.argv[2]).
   // Written data is taken from the fourth command-line argument (process.argv[3]).
 
   if (error) {
     // Handle errors that occur during the write operation.
-    console.error('Error occurred while writing the file:', error);
+    console.error("Error occurred while writing the file:", error);
   }
 });
